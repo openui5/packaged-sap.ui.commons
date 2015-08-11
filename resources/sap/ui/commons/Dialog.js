b5c4a1eecfe8e48177f56e4a8551fae47a86a08a
OpenUI5 <openui5@sap.com>
2015-08-11 10:26:38 +0200

    1.28.15

--- resources/sap/ui/commons/Dialog.js
+++ resources/sap/ui/commons/Dialog.js
@@ -22,7 +22,7 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control', 'sap/ui/
 		 * @implements sap.ui.core.PopupInterface
 		 *
 		 * @author SAP SE
-		 * @version 1.28.14
+		 * @version 1.28.15
 		 *
 		 * @constructor
 		 * @public
