b7bc4a14e6abae91fd46b709629c5f2f3ef36db0
OpenUI5 <openui5@sap.com>
2015-10-26 14:51:30 +0100

    1.28.21

--- resources/sap/ui/commons/ListBoxRenderer.js
+++ resources/sap/ui/commons/ListBoxRenderer.js
@@ -14,7 +14,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Renderer', 'jquery.sap.strings'
 	 * ListBox Renderer
 	 *
 	 * @author d046011
-	 * @version 1.28.20
+	 * @version 1.28.21
 	 * @namespace
 	 */
 	var ListBoxRenderer = {
--- resources/sap/ui/commons/PasswordField.js
+++ resources/sap/ui/commons/PasswordField.js
@@ -22,7 +22,7 @@ sap.ui.define(['jquery.sap.global', './TextField', './library'],
 	 * @extends sap.ui.commons.TextField
 	 *
 	 * @author SAP SE
-	 * @version 1.28.20
+	 * @version 1.28.21
 	 *
 	 * @constructor
 	 * @public
