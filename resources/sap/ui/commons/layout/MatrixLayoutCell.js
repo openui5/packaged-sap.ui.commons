31d7897cc1496a776ca378f49aaaba4ce30228e9
OpenUI5 <openui5@sap.com>
2015-11-23 13:21:05 +0100

    1.32.7

--- resources/sap/ui/commons/layout/MatrixLayoutCell.js
+++ resources/sap/ui/commons/layout/MatrixLayoutCell.js
@@ -23,7 +23,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/commons/library', 'sap/ui/core/Custo
 	 * @extends sap.ui.core.Element
 	 *
 	 * @author SAP SE
-	 * @version 1.32.6
+	 * @version 1.32.7
 	 *
 	 * @constructor
 	 * @public
