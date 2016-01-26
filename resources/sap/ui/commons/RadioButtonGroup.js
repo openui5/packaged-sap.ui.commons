3639351f5ccae42993b4403a892fe2503d42c51e
OpenUI5 <openui5@sap.com>
2016-01-26 10:31:50 +0100

    1.34.3

--- resources/sap/ui/commons/RadioButtonGroup.js
+++ resources/sap/ui/commons/RadioButtonGroup.js
@@ -23,7 +23,7 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control', 'sap/ui/
 	 * usage is supported.
 	 *
 	 * @extends sap.ui.core.Control
-	 * @version 1.34.2
+	 * @version 1.34.3
 	 *
 	 * @constructor
 	 * @public
