0c1036a74b86c99eb00102f403ba10c4a827c784
OpenUI5 <openui5@sap.com>
2016-03-21 14:36:14 +0100

    1.28.31

--- resources/sap/ui/commons/ButtonRenderer.js
+++ resources/sap/ui/commons/ButtonRenderer.js
@@ -12,7 +12,7 @@ sap.ui.define(['jquery.sap.global'],
 
 	/**
 	 * @author SAP SE
-	 * @version 1.28.30
+	 * @version 1.28.31
 	 * @namespace
 	 */
 	var ButtonRenderer = {
--- resources/sap/ui/commons/TreeNode.js
+++ resources/sap/ui/commons/TreeNode.js
@@ -19,7 +19,7 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/CustomStyleClassSu
 	 * @class
 	 * Tree node element
 	 * @extends sap.ui.core.Element
-	 * @version 1.28.30
+	 * @version 1.28.31
 	 *
 	 * @constructor
 	 * @public
