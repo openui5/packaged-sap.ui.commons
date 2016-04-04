f66249363a6dfd2754ae218caca4fa85f2c26185
OpenUI5 <openui5@sap.com>
2016-04-04 12:55:22 +0200

    1.36.6

--- resources/sap/ui/commons/CalloutBase.js
+++ resources/sap/ui/commons/CalloutBase.js
@@ -22,7 +22,7 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/TooltipBase'],
 	 * @extends sap.ui.core.TooltipBase
 	 *
 	 * @author SAP SE
-	 * @version 1.36.5
+	 * @version 1.36.6
 	 *
 	 * @constructor
 	 * @public
--- resources/sap/ui/commons/layout/MatrixLayout.js
+++ resources/sap/ui/commons/layout/MatrixLayout.js
@@ -41,7 +41,7 @@ sap.ui.define(['jquery.sap.global', './MatrixLayoutCell', './MatrixLayoutRow', '
 	 * @extends sap.ui.core.Control
 	 *
 	 * @author SAP SE
-	 * @version 1.36.5
+	 * @version 1.36.6
 	 *
 	 * @constructor
 	 * @public
