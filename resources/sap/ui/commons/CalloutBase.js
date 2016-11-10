69a378bb31f0ce671a306057c02c76d209459354
OpenUI5 <openui5@sap.com>
2016-11-10 22:18:51 +0100

    1.42.5

--- resources/sap/ui/commons/CalloutBase.js
+++ resources/sap/ui/commons/CalloutBase.js
@@ -22,7 +22,7 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/TooltipBase'],
 	 * @extends sap.ui.core.TooltipBase
 	 *
 	 * @author SAP SE
-	 * @version 1.42.4
+	 * @version 1.42.5
 	 *
 	 * @constructor
 	 * @public
--- resources/sap/ui/commons/TextView.js
+++ resources/sap/ui/commons/TextView.js
@@ -21,7 +21,7 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control'],
 	 * @implements sap.ui.commons.ToolbarItem
 	 *
 	 * @author SAP SE
-	 * @version 1.42.4
+	 * @version 1.42.5
 	 *
 	 * @constructor
 	 * @public
