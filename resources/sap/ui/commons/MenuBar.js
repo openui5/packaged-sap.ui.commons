10ece38cb2fdcb16ae589c0e2384adef77870b3d
OpenUI5 <openui5@sap.com>
2016-11-15 20:02:47 +0100

    1.38.11

--- resources/sap/ui/commons/MenuBar.js
+++ resources/sap/ui/commons/MenuBar.js
@@ -25,7 +25,7 @@ sap.ui.define(['jquery.sap.global', './Menu', './MenuItem', './MenuItemBase', '.
 	 * @extends sap.ui.core.Control
 	 *
 	 * @author SAP SE
-	 * @version 1.38.10
+	 * @version 1.38.11
 	 *
 	 * @constructor
 	 * @public
--- resources/sap/ui/commons/MenuTextFieldItem.js
+++ resources/sap/ui/commons/MenuTextFieldItem.js
@@ -23,7 +23,7 @@ sap.ui.define(['jquery.sap.global', './MenuItemBase', './library', 'sap/ui/unifi
 	 * @extends sap.ui.unified.MenuTextFieldItem
 	 *
 	 * @author SAP SE
-	 * @version 1.38.10
+	 * @version 1.38.11
 	 *
 	 * @constructor
 	 * @public
