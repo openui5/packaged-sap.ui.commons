0a15e724aa767289ac965934d17d64522be409b1
OpenUI5 <openui5@sap.com>
2017-02-27 17:43:53 +0100

    1.44.8

--- resources/sap/ui/commons/PasswordField.js
+++ resources/sap/ui/commons/PasswordField.js
@@ -22,7 +22,7 @@ sap.ui.define(['jquery.sap.global', './TextField', './library'],
 	 * @extends sap.ui.commons.TextField
 	 *
 	 * @author SAP SE
-	 * @version 1.44.7
+	 * @version 1.44.8
 	 *
 	 * @constructor
 	 * @public
