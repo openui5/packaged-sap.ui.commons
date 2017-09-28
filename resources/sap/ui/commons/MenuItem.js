d7671fa52282071334937a6128f9a37a246ac0e5
OpenUI5 <openui5@sap.com>
2017-09-28 14:42:58 +0200

    1.50.2

--- resources/sap/ui/commons/MenuItem.js
+++ resources/sap/ui/commons/MenuItem.js
@@ -22,7 +22,7 @@ sap.ui.define(['jquery.sap.global', './MenuItemBase', './library', 'sap/ui/unifi
 	 * @extends sap.ui.unified.MenuItem
 	 *
 	 * @author SAP SE
-	 * @version 1.50.1
+	 * @version 1.50.2
 	 * @since 1.0.0
 	 *
 	 * @constructor
--- resources/sap/ui/commons/PasswordField.js
+++ resources/sap/ui/commons/PasswordField.js
@@ -22,7 +22,7 @@ sap.ui.define(['jquery.sap.global', './TextField', './library'],
 	 * @extends sap.ui.commons.TextField
 	 *
 	 * @author SAP SE
-	 * @version 1.50.1
+	 * @version 1.50.2
 	 *
 	 * @constructor
 	 * @public
