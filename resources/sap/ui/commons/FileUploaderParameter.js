3f416c65ce2e7da33ab552ed78f6302acab62cbd
OpenUI5 <openui5@sap.com>
2017-11-09 09:51:08 +0100

    1.52.1

--- resources/sap/ui/commons/FileUploaderParameter.js
+++ resources/sap/ui/commons/FileUploaderParameter.js
@@ -20,7 +20,7 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/unified/FileUploaderPar
 	 * @extends sap.ui.unified.FileUploaderParameter
 	 *
 	 * @author SAP SE
-	 * @version 1.52.0
+	 * @version 1.52.1
 	 *
 	 * @constructor
 	 * @public
--- resources/sap/ui/commons/layout/BorderLayout.js
+++ resources/sap/ui/commons/layout/BorderLayout.js
@@ -23,7 +23,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/commons/library', 'sap/ui/core/Contr
 	 * @extends sap.ui.core.Control
 	 *
 	 * @author SAP SE
-	 * @version 1.52.0
+	 * @version 1.52.1
 	 *
 	 * @constructor
 	 * @public
