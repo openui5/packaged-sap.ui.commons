93d59760cf3c2416ed1cb7cf3220a843f364576e
OpenUI5 <openui5@sap.com>
2017-11-20 14:47:14 +0100

    1.48.14

--- resources/sap/ui/commons/Toolbar.js
+++ resources/sap/ui/commons/Toolbar.js
@@ -22,7 +22,7 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control', 'sap/ui/
 		 * @implements sap.ui.core.Toolbar
 		 *
 		 * @author SAP SE
-		 * @version 1.48.13
+		 * @version 1.48.14
 		 *
 		 * @constructor
 		 * @public
