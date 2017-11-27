b7246522f6c2860c7f2da277709507fa304c70ed
OpenUI5 <openui5@sap.com>
2017-11-27 18:58:27 +0100

    1.38.31

--- resources/sap/ui/commons/Image.js
+++ resources/sap/ui/commons/Image.js
@@ -24,7 +24,7 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control'],
 	 * @implements sap.ui.commons.ToolbarItem,sap.ui.commons.FormattedTextViewControl
 	 *
 	 * @author SAP SE
-	 * @version 1.38.30
+	 * @version 1.38.31
 	 *
 	 * @constructor
 	 * @public
