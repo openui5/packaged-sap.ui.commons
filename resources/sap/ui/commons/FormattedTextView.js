caf70dc2631ec48bcce0250c9596d31cb07a0080
OpenUI5 <openui5@sap.com>
2018-01-31 16:35:22 +0100

    1.50.9

--- resources/sap/ui/commons/FormattedTextView.js
+++ resources/sap/ui/commons/FormattedTextView.js
@@ -1,6 +1,6 @@
 /*!
  * UI development toolkit for HTML5 (OpenUI5)
- * (c) Copyright 2009-2017 SAP SE or an SAP affiliate company.
+ * (c) Copyright 2009-2018 SAP SE or an SAP affiliate company.
  * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
  */
 
@@ -19,7 +19,7 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control'],
 		 * @class
 		 * The FormattedTextView control allows the usage of a limited set of HTML tags for display.
 		 * @extends sap.ui.core.Control
-		 * @version 1.50.8
+		 * @version 1.50.9
 		 *
 		 * @constructor
 		 * @public
--- resources/sap/ui/commons/MenuItemBase.js
+++ resources/sap/ui/commons/MenuItemBase.js
@@ -1,6 +1,6 @@
 /*!
  * UI development toolkit for HTML5 (OpenUI5)
- * (c) Copyright 2009-2017 SAP SE or an SAP affiliate company.
+ * (c) Copyright 2009-2018 SAP SE or an SAP affiliate company.
  * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
  */
 
@@ -20,7 +20,7 @@ sap.ui.define(['jquery.sap.global'],
 	 * @extends sap.ui.unified.MenuItemBase
 	 *
 	 * @author SAP SE
-	 * @version 1.50.8
+	 * @version 1.50.9
 	 * @since 1.0.0
 	 *
 	 * @deprecated Since version 1.21.0.
