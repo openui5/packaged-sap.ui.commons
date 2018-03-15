2ce836e59de68d26de57c70b6c9ce7921842b7b0
OpenUI5 <openui5@sap.com>
2018-03-15 18:51:35 +0100

    1.52.9

--- resources/sap/ui/commons/ListBoxRenderer.js
+++ resources/sap/ui/commons/ListBoxRenderer.js
@@ -14,7 +14,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Renderer', 'sap/ui/core/IconPoo
 	 * ListBox Renderer
 	 *
 	 * @author SAP SE
-	 * @version 1.52.8
+	 * @version 1.52.9
 	 * @namespace
 	 */
 	var ListBoxRenderer = {
