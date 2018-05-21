16f1422f4a4c950fcef99df05494cf339ea869d4
OpenUI5 <openui5@sap.com>
2018-05-21 17:11:15 +0200

    1.52.13

--- resources/sap/ui/commons/Dialog.js
+++ resources/sap/ui/commons/Dialog.js
@@ -23,7 +23,7 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control', 'sap/ui/
 		 *
 		 * @namespace
 		 * @author SAP SE
-		 * @version 1.52.12
+		 * @version 1.52.13
 		 *
 		 * @constructor
 		 * @public
