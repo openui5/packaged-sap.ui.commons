e21e86fb152da38351e879b9150d253007b1c0a4
OpenUI5 <openui5@sap.com>
2018-05-28 18:30:08 +0200

    1.44.32

--- resources/sap/ui/commons/form/GridContainerData.js
+++ resources/sap/ui/commons/form/GridContainerData.js
@@ -23,7 +23,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/commons/library', 'sap/ui/layout/for
 	 * @extends sap.ui.layout.form.GridContainerData
 	 *
 	 * @author SAP SE
-	 * @version 1.44.31
+	 * @version 1.44.32
 	 *
 	 * @constructor
 	 * @public
--- resources/sap/ui/commons/layout/PositionContainer.js
+++ resources/sap/ui/commons/layout/PositionContainer.js
@@ -22,7 +22,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/commons/library', 'sap/ui/core/Eleme
 	 * @extends sap.ui.core.Element
 	 *
 	 * @author SAP SE
-	 * @version 1.44.31
+	 * @version 1.44.32
 	 *
 	 * @constructor
 	 * @public
