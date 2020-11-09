c331093ad27f9804f80294957b742d50229b2db0
OpenUI5 <openui5@sap.com>
2020-11-09 15:55:45 +0100

    1.38.48

--- resources/sap/ui/commons/FileUploader.js
+++ resources/sap/ui/commons/FileUploader.js
@@ -20,7 +20,7 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/unified/FileUploader'],
 	 * @extends sap.ui.unified.FileUploader
 	 *
 	 * @author SAP SE
-	 * @version 1.38.47
+	 * @version 1.38.48
 	 *
 	 * @constructor
 	 * @public
--- resources/sap/ui/commons/ValueHelpField.js
+++ resources/sap/ui/commons/ValueHelpField.js
@@ -22,7 +22,7 @@ sap.ui.define(['jquery.sap.global', './TextField', './library', 'sap/ui/core/Ico
 	 * @extends sap.ui.commons.TextField
 	 *
 	 * @author SAP SE
-	 * @version 1.38.47
+	 * @version 1.38.48
 	 *
 	 * @constructor
 	 * @public
