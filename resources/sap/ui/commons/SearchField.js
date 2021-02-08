f6e3b3a74b2f4c44450407894e966f0b10bb336d
OpenUI5 <openui5@sap.com>
2021-02-08 17:50:40 +0100

    1.52.47

--- resources/sap/ui/commons/SearchField.js
+++ resources/sap/ui/commons/SearchField.js
@@ -1,6 +1,6 @@
 /*!
  * UI development toolkit for HTML5 (OpenUI5)
- * (c) Copyright 2009-2020 SAP SE or an SAP affiliate company.
+ * (c) Copyright 2009-2021 SAP SE or an SAP affiliate company.
  * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
  */
 
@@ -48,7 +48,7 @@ sap.ui.define([
 	 * @implements sap.ui.commons.ToolbarItem
 	 *
 	 * @author SAP SE
-	 * @version 1.52.46
+	 * @version 1.52.47
 	 *
 	 * @constructor
 	 * @public
